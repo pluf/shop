7be3f5336523b326141d7b1f754995e435a6be73
hadi <hadi@dpq.co.ir>
2017-05-25 01:04:48 +0430

    some features of basic api is implemented

---
+++ src/Shop/relations.php
@@ -0,0 +1,19 @@
+<?php
+return array(
+//     'Shop_Product' => array(
+//         'relate_to' => array(
+//             'SDP_Asset',
+//             'Pluf_User',
+//             'SaaSBank_Receipt'
+//         )
+//     ),
+//     'SDP_Service' => array(
+//         'relate_to' => array(
+//             'CMS_Content',
+//             'SDP_Category'
+//         ),
+//         'relate_to_many' => array(
+//             'SDP_Asset'
+//         )
+//     )
+);
