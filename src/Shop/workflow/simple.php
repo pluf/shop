37c21f1e633f5cca9ed94db91d3e110af17c43ca
Mostafa Barmshory <mostafa.barmshory@dpq.co.ir>
2017-08-06 01:51:28 +0430

    A simple workflwo

--- src/Shop/workflow/simple.php
+++ src/Shop/workflow/simple.php
@@ -1,1 +1,25 @@
 <?php
+return array(
+        Workflow_Machine::STATE_UNDEFINED => array(
+                'next' => 'Locked'
+        ),
+        // State
+        'Locked' => array(
+                // Transaction or event
+                'coin' => array(
+                        'next' => 'Unlocked'
+                ),
+                'push' => array(
+                        'next' => 'Locked'
+                )
+        ),
+        'Unlocked' => array(
+                // Transaction or event
+                'coin' => array(
+                        'next' => 'Unlocked'
+                ),
+                'push' => array(
+                        'next' => 'Locked'
+                )
+        )
+);
