8c4444540a8ec84c17439efd4cc32f02e270ad78
hadimansouri <mohammad.hadi.mansouri@gmail.com>
2019-01-08 22:00:05 +0330

    Update sql to consider renamed fields

--- sql/V4.0.10.2__Shop_update_order_structure.sql
+++ sql/V4.0.10.2__Shop_update_order_structure.sql
@@ -5,3 +5,8 @@ ALTER TABLE `shop_order` CHANGE `payment` `payment_id` mediumint(9) unsigned DEF
 ALTER TABLE `shop_order` CHANGE `zone` `zone_id` mediumint(9) unsigned DEFAULT 0;
 ALTER TABLE `shop_order` CHANGE `agency` `agency_id` mediumint(9) unsigned DEFAULT 0;
 ALTER TABLE `shop_order` DROP COLUMN `deliver_type`;
+
+ALTER TABLE `shop_agency` CHANGE `owner` `owner_id` mediumint(9) unsigned DEFAULT 0;
+ALTER TABLE `shop_agency` CHANGE `content` `content_id` mediumint(9) unsigned DEFAULT 0;
+
+ALTER TABLE `shop_zone` CHANGE `owner` `owner_id` mediumint(9) unsigned DEFAULT 0;
