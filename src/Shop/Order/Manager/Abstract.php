a4874a612782d8bccc40a47952426d31d524da07
hadimansouri <mohammad.hadi.mansouri@gmail.com>
2019-01-20 08:24:40 +0330

    Complete process to store history for each change in state of order.

--- src/Shop/Order/Manager/Abstract.php
+++ src/Shop/Order/Manager/Abstract.php
@@ -22,16 +22,17 @@ abstract class Shop_Order_Manager_Abstract implements Shop_Order_Manager
     {
         $machine = new Workflow_Machine();
         $machine->setStates($this->getStates())
-            ->setSignals(array('Shop_Order::stateChange'))
+            ->setSignals(array(
+            'Shop_Order::stateChange'
+        ))
             ->setProperty('state')
             ->apply($order, $action);
-        if($save){
+        if ($save) {
             return $order->update();
         }
         return true;
     }
-    
-    
+
     /**
      *
      * {@inheritdoc}
@@ -50,9 +51,51 @@ abstract class Shop_Order_Manager_Abstract implements Shop_Order_Manager
         }
         return $transtions;
     }
-    
+
     /**
      * Gets list of states
      */
     abstract function getStates();
+
+    /**
+     *
+     * @param string $signal
+     * @param
+     *            $event
+     */
+    public static function addOrderHistory($signal, $event)
+    {
+        switch ($event->event) {
+            case 'set_zone':
+                $subject = $event->object->get_zone();
+                break;
+            case 'pay':
+                $subject = $event->object->get_payment();
+                break;
+            case 'set_assignee':
+                $subject = $event->object->get_assignee();
+                break;
+            case 'accept':
+            case 'reject':
+            case 'update':
+            case 'done':
+            case 'archive':
+            default:
+                $subject = $event->object;
+                break;
+        }
+
+        // Converts event name from camel case to underscored
+        $underscored = ltrim(strtolower(preg_replace('/[A-Z]([A-Z](?![a-z]))*/', '_$0', $event->event)), '_');
+
+        $history = new Shop_OrderHistory();
+        $history->order_id = $event->object;
+        $history->object_type = $event->request->user->_model;
+        $history->object_id = $event->request->user->id;
+        $history->action = $underscored;
+        $history->subject_type = $subject->_model;
+        $history->subject_id = $subject->id;
+        $history->description = '' . Pluf_Shortcuts_GetRequestParam($event->request, 'description');
+        $history->create();
+    }
 }
